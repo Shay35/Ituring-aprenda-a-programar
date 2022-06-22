d74ec7ab646b865fbf9d7e759e428b8a342bc24e
Shayra Medeiros <shayro56@gmail.com>
2022-06-22 16:33:41 -0300

    utilizando console.log

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -3,3 +3,9 @@ let idade = 36;
 let email = "shayro56@gmail.com"
 let profissao = "estudante";
 let lugarDosSonhos = "Canadá"
+
+//console.log(nome);
+//console.log(idade);
+//console.log(email);
+//console.log(profissao);
+//console.log(lugarDosSonhos);
